1949e6d9391da9ec4da1d7c3e18b5d32c6a9ac5e
Big-Smokkee <fardinfaraz13579@gmail.com>
2026-04-04 18:56:47 +0600

    updated font size

--- src/components/Characteristics/Characteristics.jsx
+++ src/components/Characteristics/Characteristics.jsx
@@ -5,38 +5,38 @@ const Characteristics = () => {
     return (
         <div className='bg-[#C4612A] flex justify-between py-6 px-14'>
             <div className='flex gap-6 items-center'>
-                <span className='font-semibold'>
+                <span className='font-semibold text-lg'>
                     <Truck></Truck>
                 </span>
                 <div>
-                    <h4 className='font-bold'>Free Delivery</h4>
+                    <h4 className='font-bold text-lg'>Free Delivery</h4>
                     <p className='font-light'>Orders above ৳999</p>
                 </div>
             </div>
             <div className='flex gap-6 items-center'>
-                <span className='font-bold'>
+                <span className='font-bold text-lg'>
                     <Package></Package>
                 </span>
                 <div>
-                    <h4 className='font-bold'>Authentic Products</h4>
+                    <h4 className='font-bold text-lg'>Authentic Products</h4>
                     <p className='font-light'>Handpicked quality</p>
                 </div>
             </div>
             <div className='flex gap-6 items-center'>
-                <span className='font-bold'>
+                <span className='font-bold text-lg'>
                     <RotateCcw></RotateCcw>
                 </span>
                 <div>
-                    <h4 className='font-bold'>Easy Returns</h4>
+                    <h4 className='font-bold text-lg'>Easy Returns</h4>
                     <p className='font-light'>7-day return policy</p>
                 </div>
             </div>
             <div className='flex gap-6 items-center'>
-                <span className='font-bold'>
+                <span className='font-bold text-lg'>
                     <ShieldCheck></ShieldCheck>
                 </span>
                 <div>
-                    <h4 className='font-bold'>Secure Payment</h4>
+                    <h4 className='font-bold text-lg'>Secure Payment</h4>
                     <p className='font-light'>100% safe checkout</p>
                 </div>
             </div>
