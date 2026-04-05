61c154fccc243929f50fe1dc696d3b0e935c56d8
Big-Smokkee <fardinfaraz13579@gmail.com>
2026-04-05 23:11:17 +0600

    useCart hook CartProvider wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
+import { CartProvider } from './components/CartContext/CartContext'
 import Categories from './components/Categories/Categories'
 import Characteristics from './components/Characteristics/Characteristics'
 import Hero from './components/Hero/Hero'
 import Navbar from './components/Navbar/Navbar'
 import Products from './components/Products/Products'
-import ProductShowcase from './components/ProductShowcase/ProductShowcase'
 
 const categoriesPromise = fetch('/categories.json').then(res => res.json())
 function App() {
@@ -12,12 +12,13 @@ function App() {
   return (
     <>
       <div className='root'>
-        <Navbar></Navbar>
-        <Hero></Hero>
-        <Characteristics></Characteristics>
-        <Categories categoriesPromise={categoriesPromise}></Categories>
-        {/* <ProductShowcase></ProductShowcase> */}
-        <Products></Products>
+        <CartProvider>
+          <Navbar></Navbar>
+          <Hero></Hero>
+          <Characteristics></Characteristics>
+          <Categories categoriesPromise={categoriesPromise}></Categories>
+          <Products></Products>
+        </CartProvider>
       </div>
     </>
   )
