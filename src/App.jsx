d9ff4b48600f5cfae1328b85fc34f00253a74899
Big-Smokkee <fardinfaraz13579@gmail.com>
2026-04-05 23:49:41 +0600

    feat: add footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { CartProvider } from './components/CartContext/CartContext'
 import Categories from './components/Categories/Categories'
 import Characteristics from './components/Characteristics/Characteristics'
+import Footer from './components/Footer/Footer'
 import Hero from './components/Hero/Hero'
 import Navbar from './components/Navbar/Navbar'
 import Newsletter from './components/Newsletter/Newsletter'
@@ -22,6 +23,7 @@ function App() {
           <Products></Products>
           <Testimonials></Testimonials>
           <Newsletter></Newsletter>
+          <Footer></Footer>
         </CartProvider>
       </div>
     </>
